9382726b11cf75e8fb5ecd8acc97a04fb3f7e3c9
Janne Suotsalo <janne.suotsalo@gmail.com>
2020-03-28 21:15:33 +0200

    working db connection

--- app.js
+++ app.js
@@ -1,18 +1,80 @@
 "use strict";
+require("dotenv").config();
+
 const express = require("express");
+const db = require("./model/db");
+const cat = require("./model/cat");
+const user = require("./model/user");
 const cors = require("cors");
+const passport = require("./utils/pass");
 const app = express();
 const bodyParser = require("body-parser");
 const port = 3000;
 
+let cats = require("./routes/catRoute");
+let users = require("./routes/userRoute");
+let authRoutes = require("./routes/authRoute.js");
+
+const loggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect("/form");
+  }
+};
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-let cats = require("./routes/catRoute");
-let users = require("./routes/userRoute");
+/*app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/form" }),
+  (req, res) => {
+    console.log("success");
+    res.redirect("/secret");
+  }
+);*/
 
-app.use("/cat", cats);
-app.use("/user", users);
+app.get("/", async (req, res) => {
+  console.log("someone visited my url");
+  res.send(await cat.find().populate("owner"));
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// temp test for cat & user creation
+app.post("/catCreate", async (req, res) => {
+  const mycat = await cat.create({
+    name: "kittyTest",
+    age: 7,
+    owner: "5e7e686ea2aa9705fc162250"
+  });
+  res.send("cat created with id: " + mycat._id);
+});
+
+app.post("/userCreate", async (req, res) => {
+  const myuser = await user.create({
+    name: "Mary",
+    email: "m@net.fi",
+    password: "abc"
+  });
+  res.send("user created with id: " + myuser._id);
+});
+
+app.get("/secret", loggedIn, (req, res) => {
+  res.render("secret");
+});
+
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+app.use("/cat", passport.authenticate("jwt", { session: false }), cats);
+app.use("/user", passport.authenticate("jwt", { session: false }), users);
+app.use("/auth", authRoutes);
+
+db.on("connected", () => {
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+});
--- models/userModel.js
+++ models/userModel.js
@@ -1,19 +1,29 @@
-'use strict';
+"use strict";
 const users = [
   {
-    id: '1',
-    name: 'John Doe',
-    email: 'john@metropolia.fi',
-    password: '1234',
+    id: "1",
+    name: "John Doe",
+    email: "john@metropolia.fi",
+    password: "1234"
   },
   {
-    id: '2',
-    name: 'Jane Doez',
-    email: 'jane@metropolia.fi',
-    password: 'qwer',
-  },
+    id: "2",
+    name: "Jane Doez",
+    email: "jane@metropolia.fi",
+    password: "qwer"
+  }
 ];
+
+const getUser = id => {
+  const user = users.filter(usr => {
+    if (usr.user_id === id) {
+      return usr;
+    }
+  });
+  return user[0];
+};
 
 module.exports = {
   users,
+  getUser
 };
